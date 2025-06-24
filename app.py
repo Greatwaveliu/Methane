fe583e322549bfa7c27e06913f7b38c5bfb8c4c6
Greatwaveliu <greatwave.liu@gmail.com>
2025-06-24 16:39:09 -0600

    Refine scatter image detection

--- app.py
+++ app.py
@@ -703,8 +703,7 @@ class MethaneAnalysisApp(ctk.CTk):
             return
             
         scatter_patterns = [
-            'scatter_*.png',
-            '*scatter*.png'
+            'scatter*.png'
         ]
         
         image_files = []
