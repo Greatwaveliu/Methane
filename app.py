f5389e5182e93a2d05ab234f6c12c3d687b1bce5
Greatwaveliu <greatwave.liu@gmail.com>
2025-06-25 08:14:32 -0600

    Restrict box plot file search

--- app.py
+++ app.py
@@ -744,8 +744,7 @@ class MethaneAnalysisApp(ctk.CTk):
             return
             
         box_patterns = [
-            '*boxplot*.png',
-            'boxplot_*.png'
+            'boxplot_*_with_mean_line.png'
         ]
         
         image_files = []
